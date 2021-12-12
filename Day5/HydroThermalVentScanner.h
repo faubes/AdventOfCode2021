b6bdfe2ca283fca62c1bf20a7ec645f318ae6406
faubes <joelfaubert@gmail.com>
2021-12-12 18:31:11 -0500

    Day 5; start day 6

---
+++ Day5/HydroThermalVentScanner.h
@@ -0,0 +1,118 @@
+﻿// AdventOfCode2021.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <set>
+#include <unordered_map>
+
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::ostream;
+using std::ifstream;
+using std::vector;
+//using std::wstring;
+using std::set;
+using std::unordered_map;
+
+
+struct Position 
+{
+	int x;
+	int y;
+
+	Position(int _x = 0, int _y = 0) : x(_x), y(_y) {}
+	
+	int Distance(Position other) 
+	{
+		return std::abs(y - other.y) + std::abs(x - other.x);
+	}
+
+	Position StepVector(Position other)
+	{
+		Position stepVector{};
+		stepVector.x = other.x == x ? 0 : (other.x - x < 0 ? -1 : 1);
+		stepVector.y = other.y == y ? 0 : (other.y - y < 0 ? -1 : 1);
+		return stepVector;
+	}
+
+	
+	Position operator+(Position other) { return Position(x + other.x, y + other.y); }
+	Position operator-(Position other) { return Position(x - other.x, y - other.y); }
+
+	
+	bool operator<(Position other) const
+	{
+		if (x == other.x)
+		{
+			return y < other.y;
+		}
+		return x < other.x;
+	}
+	
+	bool operator==(Position other) const
+	{
+		return x == other.x && y == other.y;
+	}
+
+	bool operator!=(Position other) const
+	{
+		return !operator==(other);
+	}
+
+	struct hashFunctor
+	{
+		std::size_t operator()(Position const& s) const noexcept
+		{
+			std::size_t h1 = std::hash<int>{}(s.x);
+			std::size_t h2 = std::hash<int>{}(s.y);
+			return h1 ^ (h2 << 1); // or use boost::hash_combine
+		}
+	};
+
+
+	friend ostream& operator<<(ostream& os, const Position& pos) { return os << "(" << pos.x << "," << pos.y << ")"; }
+};
+
+struct Line 
+{
+	Position a, b;
+	set<Position> GetIntermediatePoints();
+
+	bool HorizontalOrVertical()
+	{
+		return a.x == b.x || a.y == b.y;
+	}
+
+	bool operator==(const Line& other) const {
+		return a == other.a && b == other.b;
+	}
+};
+
+class HydroThermalVentScanner
+{
+private:
+	string inputFilename;
+	vector<Line> lines;
+	unordered_map<Position, int, Position::hashFunctor> intersectionMap{};
+
+public:
+
+	HydroThermalVentScanner(string _inputFilename) : inputFilename(_inputFilename) {}
+	
+	bool LoadInput();
+
+	void CalculateIntersections();
+
+	set<Position> HydroThermalVentScanner::GetIntersectionPoints();
+
+	void PrintIntersectionMap();
+};
---
+++ Day6/HydroThermalVentScanner.cpp
@@ -0,0 +1,120 @@
+﻿// AdventOfCode2021.cpp : Defines the entry point for the application.
+//
+
+#include "HydroThermalVentScanner.h"
+
+#define DEBUG 0
+
+
+set<Position> Line::GetIntermediatePoints()
+{
+	set<Position> intermediatePoints{};
+	//int distance = a.Distance(b);
+	Position p = a;
+	do 
+	{
+		intermediatePoints.insert(p);
+		p = p + a.StepVector(b);
+
+	} while (p != b);
+	intermediatePoints.insert(p); // end point
+	return intermediatePoints;
+}
+
+bool HydroThermalVentScanner::LoadInput()
+{
+	ifstream input(inputFilename);
+	if (input)
+	{
+		int x_1, x_2, y_1, y_2;
+		char comma;
+		string arrow;
+		// lazy parsing
+		while (
+			input >> x_1 && input >> comma && input >> x_2 && 
+			input >> arrow && 
+			input >> y_1 && input >> comma && input >> y_2)
+		{
+			lines.push_back(Line{ Position{x_1, x_2}, Position{y_1, y_2} });
+		}
+
+		return true;
+	}
+	cout << "Error loading input file! " << endl;
+	return false;
+}
+
+void HydroThermalVentScanner::CalculateIntersections()
+{
+	for (auto l : lines)
+	{
+		//if (l.HorizontalOrVertical())
+		//{
+			for (auto p : l.GetIntermediatePoints())
+			{
+				std::pair<unordered_map<Position, int>::iterator, bool> result = intersectionMap.insert(std::make_pair(p,1));
+				if (result.second == false) // already in hashmap
+				{
+					intersectionMap[p] += 1;
+				}
+			}
+		//}
+	}
+}
+
+set<Position> HydroThermalVentScanner::GetIntersectionPoints() {
+	set<Position> result{};
+	for (auto kv : intersectionMap)
+	{
+		if (kv.second >= 2) {
+			result.insert(kv.first);
+		}
+	}
+	return result;
+}
+void HydroThermalVentScanner::PrintIntersectionMap()
+{
+	int maxX = 0;
+	int maxY = 0;
+	for (auto kv : intersectionMap)
+	{
+		if (kv.first.x > maxX)
+			maxX = kv.first.x;
+		if (kv.first.y > maxY)
+			maxY = kv.first.y;
+	}
+	for (int i = 0; i <= maxX; i++) 
+	{
+		for (int j = 0; j <= maxY; j++)
+		{
+			try 
+			{
+				auto result = intersectionMap.at(Position{ i, j });
+				cout << result << " ";
+			}
+			catch (std::out_of_range e)
+			{
+				cout << ". ";
+			}
+
+		}
+		cout << endl;
+
+	}
+
+}
+int main(int argc, const char** arv)
+{
+	HydroThermalVentScanner htvs = HydroThermalVentScanner("..\\Day5\\input.txt");
+	htvs.LoadInput();
+	htvs.CalculateIntersections();
+	auto intersectionPoints = htvs.GetIntersectionPoints();
+#if DEBUG
+	for (auto p : intersectionPoints) {
+		cout << p << " ";
+	}
+#endif
+	cout << "There are: " << intersectionPoints.size() << " intersection points!" << endl;
+	//htvs.PrintIntersectionMap();
+	return 0;
+}
